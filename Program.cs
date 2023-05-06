65200f478a5116b3c1c28b62088a99bbcadc1a77
nuggs <71676567+thisisitgames@users.noreply.github.com>
2023-05-06 13:34:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LibraryPractice
+{
+    class Program
+    {
+        static Book[] books = new Book[10];
+        static string[] bookNames = {"To Kill a Mockingbird", "1984", "King James Bible", "All Tomrrows", "No Country for Old Men"};
+        static public Random rd;
+
+        static void Main(string[] args)
+        {
+            rd = new Random();
+
+            Console.WriteLine("Books:");
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                Book b = new Book();
+                books[i] = b;
+                NameGen(b);
+                b.Index = i;
+                Console.WriteLine(b.name + ", " + b.Index);
+            }
+        }
+
+        static void NameGen(Book book)
+        {
+            int nameIndex = rd.Next(0, bookNames.Length);
+            book.name = bookNames[nameIndex];
+        }
+    }
+
+    class Book
+    {
+        public string name = "NoName";
+
+        private int index;
+
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+    }
+}
